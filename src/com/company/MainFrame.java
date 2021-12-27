4f3929dbff45c6e259dc71961e0af6e67469bbad
Chris Konstantinou <chriskon97@gmail.com>
2021-12-27 16:25:08 +0200

    Updated main frame

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -3,8 +3,10 @@ package com.company;
 import javax.swing.*;
 
 public class MainFrame {
+    private JTextField Voc_field;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
-    private JTextField textField4;
+    private JButton plotButton;
+    private JButton clearButton;
 }
